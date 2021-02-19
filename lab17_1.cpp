138c45661160e0151e017b1218f820fcb2aca708
Nunnalin Bingbing <61963924+snnmbb@users.noreply.github.com>
2021-02-19 18:50:00 +0700

    finished

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -20,20 +20,83 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
+void importDataFromFile(string filename,vector<string> &names,vector<int> &scores,vector<char> &grades)
+{
 
+    ifstream source;
+    string text;
+    source.open(filename);
+    while (getline(source,text))
+    {
+        char format[] = "%[^:]: %d %d %d";
+        char name[100];
+        int a,b,c;
+        sscanf(text.c_str(),format,name,&a,&b,&c);
+        int score=a+b+c;
+        names.push_back(name); 
+        scores.push_back(score);
+        grades.push_back(score2grade(score));
+
+    } 
 }
 
-void getCommand(){
 
+void getCommand(string &command,string &key)
+{
+    cout<<"Please input your command: ";
+    getline(cin,command);
+    char format[] ="%s %[^\n]";
+    char text_1[100];
+    char text_2[100];
+    sscanf(command.c_str(),format,text_1,text_2);
+    command=text_1;
+    key=text_2;
 }
 
-void searchName(){
+void searchName(vector<string> names,vector<int> scores,vector<char> grades,string key)
+{
+    bool found = 0;
+    cout<<"---------------------------------"<<"\n";
+    
+    for(unsigned int i = 0;i<names.size();i++)
+    {
+          
+        if(toUpperStr(names[i]) ==key)
+        {
+            found = 1;
+            cout<<names[i]<<"'s "<<"score "<<"= "<<scores[i]<<"\n";
+            cout<<names[i]<<"'s "<<"grade "<<"= "<<grades[i]<<"\n";
+        }
+        
+    }
+    if (found == 0)
+    {
+            cout<<"Cannot found.\n"; 
 
+    }
+    
+    cout<<"---------------------------------"<<"\n";
+    
 }
 
-void searchGrade(){
+void searchGrade(vector<string> names,vector<int> scores,vector<char> grades,string key)
+{
+     bool found = 0;
+    cout<<"---------------------------------"<<"\n";
+    for(unsigned int i = 0; i<names.size();i++)
+    {
+        if(grades[i]==key[0])
+        {
+            found = 1;
+            cout<<names[i]<<" "<<"("<<scores[i]<<")"<<"\n";
+        }
+    }
+        if (found == 0)
+    {
+            cout<<"Cannot found.\n"; 
 
+    }
+    cout<<"---------------------------------"<<"\n";
 }
 
 
